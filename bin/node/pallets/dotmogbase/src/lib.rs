d6af719ec43ab27058b0d6423d1348d5d35c0128
Cedric Decoster <cedric.decoster@gmail.com>
2021-04-11 18:42:42 +0200

    added more function for claim processing updated to spec 5

--- bin/node/pallets/dotmogbase/src/lib.rs
+++ bin/node/pallets/dotmogbase/src/lib.rs
@@ -161,10 +161,12 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T:Config> Pallet<T> {
+
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResultWithPostInfo {
+
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
